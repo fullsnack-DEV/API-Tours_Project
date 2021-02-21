f6f17d87afa9652c8889e3f3efe1a700551e1b50
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-02-21 12:22:19 +0530

    connected to server

--- server.js
+++ server.js
@@ -21,6 +21,42 @@ mongoose
     console.log('db connection successful!');
   });
 
+//creating a tour schema
+
+const tourschems = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price'],
+  },
+});
+//building a model
+
+const Tour = mongoose.model('Tour', tourschems);
+
+const testtour = new Tour({
+  name: 'The forest dancer',
+});
+
+testtour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err);
+  });
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`The server is listnning at ${port}`);
