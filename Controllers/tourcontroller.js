2010862fce478eff1b50f83fe0e9ff6958c40e98
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-02-28 04:12:12 +0530

    create operation on tour

--- Controllers/tourcontroller.js
+++ Controllers/tourcontroller.js
@@ -48,7 +48,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failed',
-      message: err,
+      message: 'invalid data set',
     });
   }
 };
