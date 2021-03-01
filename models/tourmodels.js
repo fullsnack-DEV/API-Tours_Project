6e11635ce1fe8dec04bd0b5ab30c2aa4826d464a
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-03-01 15:04:19 +0530

    created a mongoose schema for data

--- models/tourmodels.js
+++ models/tourmodels.js
@@ -5,6 +5,38 @@ const tourschems = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
   },
 
   rating: {
@@ -14,9 +46,22 @@ const tourschems = new mongoose.Schema({
 
   price: {
     type: Number,
-
     required: [true, 'Tour must have a price'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A image must have a name'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 //building a model
 
